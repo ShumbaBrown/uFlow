58417a8b827baf34c759d0a1885d7aab12ad994f
Shumba Brown <shumbabrownjc@gmail.com>
2017-04-27 14:41:11 -0400

    stuff

--- insert.php
+++ insert.php
@@ -10,8 +10,12 @@ li {listt-style: none;}
 <h2>Enter information regarding book</h2>
 <ul>
 <form name="insert" action="insert.php" method="POST" >
-<li>questionID:</li><li><input type="text" name="questionid" /></li>
-<li>answer:</li><li><input type="text" name="answer" /></li>
+<li>Book ID:</li><li><input type="text" name="bookid" /></li>
+<li>Book Name:</li><li><input type="text" name="book_name" /></li>
+<li>Author:</li><li><input type="text" name="author" /></li>
+<li>Publisher:</li><li><input type="text" name="publisher" /></li>
+<li>Date of publication:</li><li><input type="text" name="dop" /></li>
+<li>Price (USD):</li><li><input type="text" name="price" /></li>
 <li><input type="submit" /></li>
 </form>
 </ul>
@@ -19,7 +23,8 @@ li {listt-style: none;}
 </html>
 <?php
 $db = pg_connect("host=ec2-54-204-0-88.compute-1.amazonaws.com port=5432 dbname=d3hnfidbth4s7m user=socgheqnrisjpe password=2b039a1b59f3a43a62a998c5b756574ca8d4ce660eb7a068613ccd695cc0e60a");
-$query = "INSERT INTO answers VALUES ('$_POST[questionid]','$_POST[questionid]',
-'$_POST[answer]')";
+$query = "INSERT INTO book VALUES ('$_POST[bookid]','$_POST[book_name]',
+'$_POST[author]','$_POST[publisher]','$_POST[dop]',
+'$_POST[price]')";
 $result = pg_query($query); 
 ?>
